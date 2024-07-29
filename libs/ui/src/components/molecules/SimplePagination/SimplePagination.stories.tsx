76a0210cf99d876a45ac50dd4eec2d7268036e4f
Naveed Waddo <naveedwaddo1123@gmail.com>
2024-07-29 13:30:01 +0530

    update prod script

--- libs/ui/src/components/molecules/SimplePagination/SimplePagination.stories.tsx
+++ libs/ui/src/components/molecules/SimplePagination/SimplePagination.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { SimplePagination } from './SimplePagination'
-import { useTakeSkip } from '@haveyouseen-org/util'
+import { useTakeSkip } from '@findus-org/util'
 
 const meta: Meta<typeof SimplePagination> = {
   component: SimplePagination,
